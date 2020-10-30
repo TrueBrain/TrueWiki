010c2392031cb1ecfd27c3a47c1606c7dc8d6e99
Patric Stout <truebrain@openttd.org>
2020-10-30 11:50:01 +0100

    Codechange: move "create" functions into their own folder and files

---
+++ truewiki/create/category_bar.py
@@ -0,0 +1,15 @@
+from wikitexthtml.render import wikilink
+import wikitextparser
+
+from ..wiki_page import WikiPage
+
+
+def create(page, categories):
+    categories_content = set()
+    for category in categories:
+        label = "/".join(category.split("/")[1:])
+        categories_content.add(f"<li>[[:Category:{category}|{label}]]</li>")
+
+    wtp = wikitextparser.parse("\n".join(sorted(categories_content)))
+    wikilink.replace(WikiPage(page), wtp)
+    return '<div id="categories"><div>Categories:</div><ul>' + wtp.string + "</ul></div>"
