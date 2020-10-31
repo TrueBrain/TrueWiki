40ac9b680edd11ca39affdc9b7300eb09f1299ee
Patric Stout <truebrain@openttd.org>
2020-10-31 16:59:14 +0100

    Add: allow editing of pages and creation of new pages (#12)

--- truewiki/render.py
+++ truewiki/render.py
@@ -10,6 +10,7 @@ from .create import (
     folder_index,
     language_bar,
 )
+from . import singleton
 from .user_session import (
     get_user_method,
     get_user_methods,
@@ -34,6 +35,85 @@ def render_login(user) -> str:
     return wrap_page("Login", "Login", variables, templates)
 
 
+def save_edit(user, page: str, change: str) -> None:
+    # TODO -- Move this function away from "render.py"
+    # TODO -- Recalculate Translations and Categories
+    wiki_page = WikiPage(page)
+
+    filename = wiki_page.page_ondisk_name(page)
+    with open(f"{singleton.STORAGE.folder}/{filename}", "w") as fp:
+        fp.write(change)
+
+
+def render_edit(user, page: str) -> str:
+    wiki_page = WikiPage(page)
+    body = wiki_page.page_load(page)
+    wikipage = WikiPage(page).render()
+
+    templates_used = [f"<li>[[:Template:{template}]]</li>" for template in wikipage.templates]
+    errors = [f"<li>{error}</li>" for error in wikipage.errors]
+
+    wtp = wikitextparser.parse("\n".join(sorted(templates_used)))
+    wikilink.replace(WikiPage(page), wtp)
+    templates_used = wtp.string
+
+    templates = {
+        "page": body,
+        "templates_used": templates_used,
+        "errors": "\n".join(errors),
+        "breadcrumbs": breadcrumb.create(page),
+    }
+    variables = {
+        "has_templates_used": "1" if templates_used else "",
+        "has_errors": "1" if errors else "",
+        "display_name": user.display_name if user else "",
+    }
+
+    return wrap_page(page, "Edit", variables, templates)
+
+
+def render_preview(user, page: str, body: str) -> str:
+    if page.endswith("/"):
+        page += "Main Page"
+
+    wikipage = WikiPage(page)
+    wtp = wikipage.prepare(body)
+    content = wikipage.render_page(wtp)
+    errors = [f"<li>{error}</li>" for error in wikipage.errors]
+
+    templates = {
+        "content": content,
+        "page": body,
+        "language": "",
+        "footer": "",
+        "errors": "\n".join(errors),
+        "breadcrumbs": breadcrumb.create(page),
+    }
+    variables = {
+        "display_name": user.display_name if user else "",
+        "has_errors": "1" if errors else "",
+    }
+
+    if wikipage.en_page:
+        templates["language"] = language_bar.create(page, wikipage.en_page)
+    if wikipage.categories:
+        templates["footer"] += category_bar.create(page, wikipage.categories)
+
+    if page.startswith("Category/"):
+        templates["content"] += category_index.create(page)
+        templates["footer"] += folder_bar.create(page, "Category")
+    elif page.startswith("Folder/"):
+        templates["content"] += folder_index.create(page)
+    elif page.startswith("Template/"):
+        templates["footer"] += folder_bar.create(page, "Template")
+        if page == "Template/Main Page":
+            templates["content"] += folder_index.create("Folder/Template/Main Page", namespace="Template")
+    else:
+        templates["footer"] += folder_bar.create(page, "Page")
+
+    return wrap_page(page, "Preview", variables, templates)
+
+
 def render_source(user, page: str) -> str:
     body = WikiPage(page).page_load(page)
     wikipage = WikiPage(page).render()
@@ -46,7 +126,7 @@ def render_source(user, page: str) -> str:
     templates_used = wtp.string
 
     templates = {
-        "content": body,
+        "page": body,
         "templates_used": templates_used,
         "errors": "\n".join(errors),
         "breadcrumbs": breadcrumb.create(page),
