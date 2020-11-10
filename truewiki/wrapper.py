21fc1ba7a94631050857399f72a042cc2e469cf2
Patric Stout <truebrain@openttd.org>
2020-11-10 10:48:31 +0100

    Fix: don't try to process pagenames on error pages
    The reason it is an error page can be because the history or
    source or what-ever could not be generated. As such, this can
    cause an error while rendering an error.

--- truewiki/wrapper.py
+++ truewiki/wrapper.py
@@ -18,14 +18,15 @@ def wrap_page(page, wrapper, variables, templates):
     body = preprocess.begin(wiki_page, body)
     wtp = wikitextparser.parse(body)
 
-    if wiki_page.has_history(page):
-        filename = wiki_page.page_ondisk_name(page)
-        if filename:
-            variables["history_url"] = singleton.STORAGE.get_history_url(filename)
-    variables["has_source"] = "1" if wiki_page.has_source(page) else ""
-    variables["does_exist"] = "1" if wiki_page.has_history(page) else ""
-    variables["create_page_name"] = wiki_page.get_create_page_name(page)
-    variables["repository_url"] = singleton.STORAGE.get_repository_url()
+    if wrapper != "Error":
+        if wiki_page.has_history(page):
+            filename = wiki_page.page_ondisk_name(page)
+            if filename:
+                variables["history_url"] = singleton.STORAGE.get_history_url(filename)
+        variables["has_source"] = "1" if wiki_page.has_source(page) else ""
+        variables["does_exist"] = "1" if wiki_page.has_history(page) else ""
+        variables["create_page_name"] = wiki_page.get_create_page_name(page)
+        variables["repository_url"] = singleton.STORAGE.get_repository_url()
 
     arguments = [wikitextparser.Argument(f"|{name}={value}") for name, value in variables.items()]
     parameter.replace(wiki_page, wtp, arguments)
