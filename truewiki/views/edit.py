955e300df714ecd249f993246c7cb189360fb608
Patric Stout <truebrain@openttd.org>
2020-11-10 15:14:43 +0100

    Fix #48: report errors about wrong page names within the edit page (#63)
    This should make it easier to see and fix any page name error.

--- truewiki/views/edit.py
+++ truewiki/views/edit.py
@@ -17,23 +17,21 @@ from ..wiki_page import WikiPage
 def save(user, old_page: str, new_page: str, content: str, payload) -> web.Response:
     wiki_page = WikiPage(old_page)
     page_error = wiki_page.page_is_valid(old_page, is_new_page=True)
-    if not page_error:
+    if page_error:
+        return error.view(user, old_page, page_error)
+
+    if new_page.endswith("/"):
+        page_error = (
+            f'Page name "{new_page}" cannot end with a "/". If you meant the main page, please add "Main Page". '
+            'The name of the page "Main Page" cannot be translated, and is always written in English, '
+            "no matter the language you are in. The content of course can be translated."
+        )
+    else:
         page_error = wiki_page.page_is_valid(new_page, is_new_page=True)
-    if not page_error:
-        if new_page.endswith("/"):
-            page_error = (
-                f'Page name "{new_page}" cannot end with a "/". If you meant the main page, please add "Main Page". '
-                'The name of the page "Main Page" cannot be translated, and is always written in English, '
-                "no matter the language you are in. The content of course can be translated."
-            )
 
     if page_error:
-        return error.view(
-            user,
-            old_page,
-            page_error,
-            status=401,
-        )
+        body = source.create_body(wiki_page, user, "Edit", new_page=new_page, page_error=page_error)
+        return web.Response(body=body, content_type="text/html")
 
     # If the old page doesn't exist, this creates a page. But it is possible
     # that this is done from an old page with a different name. Make sure the
--- truewiki/views/preview.py
+++ truewiki/views/preview.py
@@ -7,11 +7,20 @@ from . import (
 from ..wiki_page import WikiPage
 
 
-def view(user, page: str, new_page: str, body: str) -> web.Response:
-    wiki_page = WikiPage(page)
-    page_error = wiki_page.page_is_valid(page)
+def view(user, old_page: str, new_page: str, body: str) -> web.Response:
+    wiki_page = WikiPage(old_page)
+    page_error = wiki_page.page_is_valid(old_page, is_new_page=True)
     if page_error:
-        return error.view(user, page, page_error)
+        return error.view(user, old_page, page_error)
 
-    body = source.create_body(wiki_page, user, "Edit", preview=body, new_page=new_page)
+    if new_page.endswith("/"):
+        page_error = (
+            f'Page name "{new_page}" cannot end with a "/". If you meant the main page, please add "Main Page". '
+            'The name of the page "Main Page" cannot be translated, and is always written in English, '
+            "no matter the language you are in. The content of course can be translated."
+        )
+    else:
+        page_error = wiki_page.page_is_valid(new_page, is_new_page=True)
+
+    body = source.create_body(wiki_page, user, "Edit", preview=body, new_page=new_page, page_error=page_error)
     return web.Response(body=body, content_type="text/html")
