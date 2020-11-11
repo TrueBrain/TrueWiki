0f9475990adb59f82475ca974d9eb875a36a4b1d
Patric Stout <truebrain@openttd.org>
2020-11-11 10:26:34 +0100

    Add: support [[Media:NNN]] links
    This creates a link to the raw image, not to the HTML page.
    
    Fixes #67.

--- truewiki/namespaces/file/wikilink.py
+++ truewiki/namespaces/file/wikilink.py
@@ -1,14 +1,17 @@
+import html
 import logging
+import urllib
 import wikitextparser
 
 from wikitexthtml.render import wikilink
+from wikitexthtml.exceptions import InvalidWikiLink
 
 from ...wiki_page import WikiPage
 
 log = logging.getLogger(__name__)
 
 
-def link(instance: WikiPage, wikilink: wikitextparser.WikiLink):
+def link_file(instance: WikiPage, wikilink: wikitextparser.WikiLink):
     target = wikilink.target
     target = target[len(":file:") :]
 
@@ -22,5 +25,46 @@ def link(instance: WikiPage, wikilink: wikitextparser.WikiLink):
     return False
 
 
-# [[:Template]] and [[Template]] have the same meaning.
-wikilink.register_namespace(":file", link)
+def link_media(instance: WikiPage, wikilink: wikitextparser.WikiLink):
+    target = wikilink.target
+    target = target[len("media:") :]
+
+    url = f"File/{target}"
+
+    text = ""
+    # Always run clean_title() on the URL, as it detects URLs that are invalid
+    # because of invalid namespaces, etc.
+    if url:
+        try:
+            text = instance.clean_title(url)
+        except InvalidWikiLink as e:
+            # Errors always end with a dot, hence the [:-1].
+            instance.add_error(f'{e.args[0][:-1]} (wikilink "{wikilink.string}").')
+            return
+
+    if wikilink.text:
+        text = wikilink.text.strip()
+
+    title = html.escape(wikilink.target)
+    text = html.escape(text)
+
+    url = instance.clean_url(url)
+
+    if url and not instance.page_exists(url):
+        instance.add_error(f'Linked page "{wikilink.title}" does not exist (wikilink "{wikilink.string}").')
+
+        url = f"/{urllib.parse.quote(url)}"
+        wikilink.string = f'<a href="{url}" class="new" title="{title}">{title}</a>'
+        return True
+
+    if url:
+        url = "uploads/" + url[len("File/") :]
+        url = f"/{urllib.parse.quote(url)}"
+
+    wikilink.string = f'<a href="{url}" title="{title}">{text}</a>'
+    return True
+
+
+# [[File]] is already handled by wikitexthtml
+wikilink.register_namespace(":file", link_file)
+wikilink.register_namespace("media", link_media)
