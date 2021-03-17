799abe8664119b4f2b38f5ffae43b344fa398ca3
Patric Stout <truebrain@openttd.org>
2021-03-17 11:59:01 +0100

    Add: GitLab as storage backend

---
+++ truewiki/storage/gitlab.py
@@ -0,0 +1,98 @@
+import base64
+import click
+import logging
+import tempfile
+import urllib
+
+from openttd_helpers import click_helper
+
+from .git import Storage as GitStorage
+from .github import OutOfProcessStorage as GitHubOutOfProcessStorage
+
+log = logging.getLogger(__name__)
+
+_gitlab_private_key = None
+_gitlab_url = None
+_gitlab_history_url = None
+
+
+class OutOfProcessStorage(GitHubOutOfProcessStorage):
+    name = "GitLab"
+
+
+class Storage(GitStorage):
+    out_of_process_class = OutOfProcessStorage
+
+    def __init__(self):
+        super().__init__()
+
+        # We need to write the private key to disk: GitPython can only use
+        # SSH-keys that are written on disk.
+        if _gitlab_private_key:
+            self._gitlab_private_key_file = tempfile.NamedTemporaryFile()
+            self._gitlab_private_key_file.write(_gitlab_private_key)
+            self._gitlab_private_key_file.flush()
+
+            self._ssh_command = f"ssh -i {self._gitlab_private_key_file.name}"
+
+    def prepare(self):
+        _git = super().prepare()
+
+        # Make sure the origin is set correctly
+        if "origin" not in _git.remotes:
+            _git.create_remote("origin", _gitlab_url)
+        origin = _git.remotes.origin
+        if origin.url != _gitlab_url:
+            origin.set_url(_gitlab_url)
+
+        return _git
+
+    def reload(self):
+        self._run_out_of_process(self._reload_done, "fetch_latest")
+
+    def _reload_done(self):
+        super().reload()
+
+    def commit_done(self):
+        self._run_out_of_process(None, "push")
+
+    def get_history_url(self, page):
+        page = urllib.parse.quote(page)
+        return f"{_gitlab_history_url}/-/commits/master/{page}"
+
+    def get_repository_url(self):
+        return _gitlab_history_url
+
+
+@click_helper.extend
+@click.option(
+    "--storage-gitlab-url",
+    help="Repository URL on Gitlab.",
+    default="https://gitlab.com/TrueBrain/wiki-example.git/",
+    show_default=True,
+    metavar="URL",
+)
+@click.option(
+    "--storage-gitlab-history-url",
+    help="Repository URL on Gitlab to visit history (defaults to --storage-gitlab-url).",
+    default=None,
+    show_default=True,
+    metavar="URL",
+)
+@click.option(
+    "--storage-gitlab-private-key",
+    help="Base64-encoded private key to access Gitlab." "Always use this via an environment variable!",
+)
+def click_storage_gitlab(storage_gitlab_url, storage_gitlab_history_url, storage_gitlab_private_key):
+    global _gitlab_url, _gitlab_history_url, _gitlab_private_key
+
+    if storage_gitlab_history_url is None:
+        storage_gitlab_history_url = storage_gitlab_url
+    # Normally GitLab URLs end on ".git/"; so be nice, and replace that to become a valid URL for humans.
+    if storage_gitlab_history_url.endswith(".git/"):
+        storage_gitlab_history_url = storage_gitlab_history_url[:-5] + "/"
+
+    _gitlab_url = storage_gitlab_url
+    _gitlab_history_url = storage_gitlab_history_url
+    if storage_gitlab_private_key:
+        _gitlab_private_key = base64.b64decode(storage_gitlab_private_key)
