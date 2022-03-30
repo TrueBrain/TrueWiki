e74c94d75becff53ee8699b641f0eac25365a5b4
Quentin <quentin@quentb.com>
2022-03-30 16:22:37 -0400

    Fix: file collision breaks on files that don't exist
    whoops we need to account for path

--- truewiki/storage/local.py
+++ truewiki/storage/local.py
@@ -37,7 +37,11 @@ class Storage:
         return ""
 
     def get_file_nonce(self, filename: str) -> str:
-        return str(os.path.getmtime(f"{self._folder}/{filename}"))
+        return (
+            str(os.path.getmtime(f"{self._folder}/{filename}"))
+            if self.file_exists(f"{self._folder}/{filename}")
+            else "0"
+        )
 
     def file_exists(self, filename: str) -> bool:
         return os.path.exists(f"{self._folder}/{filename}")
